9314a4d8edd4f54006b08f73c71ef8f35e090ad3
phon42 <owenbchanas@gmail.com>
2025-12-16 08:36:36 -0600

    Updated Decimal

--- Decimal.java
+++ Decimal.java
@@ -1,3 +1,43 @@
-public class Decimal extends Number implements Comparable<Decimal> {
+import java.math.BigInteger;
 
+public class Decimal extends Number implements Comparable<Decimal> {
+    // Required properties
+    private BigInteger intVal;
+    private BigInteger scale;
+
+    // Reference properties
+    public static final Decimal ZERO =
+        new Decimal(BigInteger.ZERO, BigInteger.ZERO);
+    public static final Decimal ONE =
+        new Decimal(BigInteger.ONE, BigInteger.ZERO);
+    public static final Decimal TWO =
+        new Decimal(BigInteger.TWO, BigInteger.ZERO);
+    public static final Decimal TEN =
+        new Decimal(BigInteger.TEN, BigInteger.ZERO);
+
+    public Decimal(BigInteger intVal, BigInteger scale) {
+        setScale(scale);
+        setIntVal(intVal);
+    }
+
+    // getIntVal and getScale purposefully removed
+    private void setIntVal(BigInteger intVal) {
+        if (intVal == null) {
+            throw new IllegalArgumentException("intVal is null");
+        }
+        this.intVal = intVal;
+    }
+    private void setScale(BigInteger scale) {
+        if (scale == null) {
+            throw new IllegalArgumentException("scale is null");
+        }
+        this.scale = scale;
+    }
+
+    public BigInteger unscaledValue() {
+        return intVal;
+    }
+    public BigInteger scale() {
+        return scale;
+    }
 }
